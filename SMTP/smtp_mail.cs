2d5e52647806a6650957dcaeb5addcc7f817f93f
Elvin Suleymanov <97979588+suleymanovdev@users.noreply.github.com>
2023-03-19 17:55:41 +0400

    SMTP

---
+++ SMTP/smtp_mail.cs
@@ -0,0 +1,37 @@
+using System.Net.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using MailKit.Search; 
+
+namespace SMTP_MAIL
+{
+    internal class Program
+    {
+        static void SMTP()
+        {
+            var client = new SmtpClient("your mail smtp", 587);
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential(
+                    "your email",
+                    "your password"
+                );
+            var message = new MailMessage
+            {
+                Body = "your text",
+                Subject = "your text"
+            };             
+            message.From = new MailAddress("your email", "your name");
+            message.To.Add(new MailAddress("sending email"));
+            client.Send(message);
+        }         
+
+        static void Main(string[] args)
+        {
+            SMTP();
+        }
+    }
+}
